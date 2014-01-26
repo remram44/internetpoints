112a8c134ce9550f6f060b05c561686ab1313627
Remi Rampin <remirampin@gmail.com>
2014-01-26 12:43:02 -0500

    Moves SQLAlchemy init code out of web.py

---
+++ internetpoints/storage.py
@@ -0,0 +1,11 @@
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+
+from . import config, models
+
+
+# Setup SQLAlchemy
+engine = create_engine(config.DATABASE_URI)
+Session = sessionmaker(bind=engine)
+if not engine.dialect.has_table(engine.connect(), 'threads'):
+    models.Base.metadata.create_all(bind=engine)
