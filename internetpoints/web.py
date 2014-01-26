451676a07b43215da57bd94bcae260c513300c6c
Remi Rampin <remirampin@gmail.com>
2014-01-26 15:57:13 -0500

    Implements thread view and voting

--- internetpoints/web.py
+++ internetpoints/web.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from sqlalchemy.exc import IntegrityError
 
 from . import models
 from .storage import Session
@@ -39,3 +40,35 @@ def vote():
                       .order_by(models.Thread.last_msg.desc())
                       .limit(50)).all()
     return render_template('vote.html', threads=threads)
+
+
+@app.route('/thread/<int:thread_id>')
+def thread(thread_id):
+    """Shows a thread and allows to vote on it.
+    """
+    session = Session()
+    thread = (session.query(models.Thread)
+                     .filter(models.Thread.id == thread_id)).one()
+    tasks = (session.query(models.Task)).all()
+    return render_template('thread.html', thread=thread, tasks=tasks)
+
+
+@app.route('/assign_task/<int:thread_id>', methods=['POST'])
+def assign_task(thread_id):
+    """Assign a new task to a thread.
+    """
+    session = Session()
+    thread = (session.query(models.Thread)
+                     .filter(models.Thread.id == thread_id)).one()
+    task = (session.query(models.Task)
+                     .filter(models.Task.id == request.form['task'])).one()
+
+    try:
+        task_assignation = models.TaskAssignation(
+                thread=thread,
+                task=task)
+        session.add(task_assignation)
+        session.commit()
+    except IntegrityError:
+        pass
+    return redirect(url_for('thread', thread_id=thread_id))
