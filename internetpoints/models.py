4b889b8f0b1f8dc11dad104a3b75df6daaa3be38
Remi Rampin <remirampin@gmail.com>
2014-01-26 14:27:49 -0500

    Adds Task model

--- internetpoints/models.py
+++ internetpoints/models.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import ForeignKey
-from sqlalchemy.types import Integer, String, Text
+from sqlalchemy.types import Integer, String, Text, DateTime
 
 
 Base = declarative_base()
@@ -32,6 +33,8 @@ class Thread(Base):
     id = Column(Integer, primary_key=True)
 
     messages = relationship('Message', back_populates='thread')
+    task_assignations = relationship('TaskAssignation',
+                                     back_populates='thread')
 
 
 class Message(Base):
@@ -43,3 +46,25 @@ class Message(Base):
 
     thread_id = Column(Integer, ForeignKey('threads.id'), nullable=False)
     thread = relationship('Thread',  back_populates='messages')
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    reward = Column(Integer, nullable=False, default=0)
+
+    assignations = relationship('TaskAssignation', back_populates='task')
+
+
+class TaskAssignation(Base):
+    __tablename__ = 'thread_tasks'
+
+    # Date is UTC!
+    date = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+    thread_id = Column(Integer, ForeignKey('threads.id'), primary_key=True)
+    thread = relationship('Thread', back_populates='task_assignations')
+    task_id = Column(Integer, ForeignKey('tasks.id'), primary_key=True)
+    task = relationship('Task', back_populates='assignations')
