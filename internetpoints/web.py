f699a64181e86b580da6185659bc2f93fdd51842
Remi Rampin <remirampin@gmail.com>
2014-03-15 20:57:33 -0400

    Adds logic to register posters or add emails

--- internetpoints/web.py
+++ internetpoints/web.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template, request, Response, url_for
 import functools
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import joinedload
+from sqlalchemy.orm.exc import NoResultFound
 
 from internetpoints import config, models
 from internetpoints.storage import Session
@@ -87,8 +88,14 @@ def thread(thread_id):
     posters = set(msg.poster_email.poster
                   for msg in thread.messages
                   if msg.poster_email is not None)
+    # The email addresses participating in this thread but not yet associated
+    # to a Poster
+    registerable_senders = [msg.from_
+                            for msg in thread.messages
+                            if not msg.poster_email]
     return render_template('thread.html',
-                           thread=thread, tasks=tasks, posters=posters)
+                           thread=thread, tasks=tasks, posters=posters,
+                           registerable_senders=registerable_senders)
 
 
 @app.route('/assign_task/<int:thread_id>', methods=['POST'])
@@ -121,3 +128,57 @@ def assign_task(thread_id):
     except IntegrityError:
         pass
     return redirect(url_for('thread', thread_id=thread_id))
+
+
+@app.route('/add_email', methods=['POST'])
+@requires_auth
+def add_email():
+    """Create a new Poster or add an address to an existing Poster.
+    """
+    session = Session()
+    email = request.form['email']
+    # Look up a Poster with that email
+    poster_email = (session.query(models.PosterEmail)
+                           .options(
+                               joinedload(models.PosterEmail.poster))
+                           .filter(models.PosterEmail.address == email))
+    try:
+        poster_email = poster_email.one()
+    except NoResultFound:
+        pass
+    else:
+        return redirect(url_for(
+                'edit_poster', poster=poster_email.poster.id,
+                error="This email is already associated to a poster"))
+
+    # If we got poster the info to create a new poster
+    if 'name' in request.form and 'poster_id' not in request.form:
+        new_poster = models.Poster(name=request.form['name'])
+        session.add(new_poster)
+        new_email = models.PosterEmail(address=email, poster=new_poster)
+        session.add(new_email)
+        session.commit()
+        return redirect(url_for('edit_poster', poster_id=new_poster.id,
+                                msg='Poster created'))
+    elif 'name' not in request.form and 'poster_id' in request.form:
+        poster_id = int(request.form['poster_id'])
+        poster = (session.query(models.Poster)
+                         .filter(models.Poster.id == poster_id)).one()
+        new_email = models.PosterEmail(address=email, poster=poster)
+        session.add(new_email)
+        session.commit()
+        return redirect(url_for('edit_poster', poster_id=poster.id))
+    # If both are set, something is going on, just display the forms again
+
+    # Renders the form that will allow to choose an existing Poster or to
+    # create a new one
+    # In both cases, redirect here
+    all_posters = session.query(models.Poster).all()
+    return render_template('add_email.html', email=email,
+                           posters=all_posters)
+
+
+@app.route('/edit_poster/<int:poster_id>')
+@requires_auth
+def edit_poster(poster_id):
+    return Response("TODO", content_type='text/plain')
